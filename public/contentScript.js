df494aa5cd7b62fc2cda581769cf1fc4aaed895a
billa05 <workinguse5@gmail.com>
2025-02-22 14:43:03 +0530

    removed some stuffs

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,18 +1,12 @@
 let sidebar = document.querySelector('div[data-testid="issue-viewer-metadata-container"]');
 
-window.gitGuruAssets = {
-    viteLogo: chrome.runtime.getURL("vite.svg"),
-    reactLogo: chrome.runtime.getURL("react.svg")
-};
-
 if (sidebar) {
     let div = document.createElement("div");
-    div.id = "git-guru-root"; // Unique ID for mounting React
+    div.id = "git-guru-root";
     sidebar.insertBefore(div, sidebar.firstChild);
 
-    // Inject the React app dynamically
     let script = document.createElement("script");
-    script.src = chrome.runtime.getURL("index.js"); // Load the bundled React app
+    script.src = chrome.runtime.getURL("index.js");
     script.type = "module";
     document.body.appendChild(script);
 }
